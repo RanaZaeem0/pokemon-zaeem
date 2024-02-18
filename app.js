e60eb831bc3747cbd99c364fd24057097b77b458
zainibhai333333@gmail.com <zainibhai333333@gmail.com>
2024-02-18 19:46:58 +0500

    add

--- app.js
+++ app.js
@@ -9,4 +9,31 @@
 //         })
 //     })
 // }
-// bounce()
+// bounce()(
+
+// var num  = 10%3;
+// console.log(num)
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+})
