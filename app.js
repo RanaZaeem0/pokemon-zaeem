3bc00552b3e8808451c6a5988abe819657742b8d
Rana Zaeem <149497053+RanaZaeem0@users.noreply.github.com>
2024-02-22 23:01:52 +0500

    addd

--- app.js
+++ app.js
@@ -42,3 +42,27 @@ $(document).ready(function(){
     }
 })
 })
+
+let navConatiner = document.getElementsByClassName("container-fluid")
+let lastScrollTop =
+  window.pageYOffset || document.documentElement.scrollTop;
+
+window.addEventListener(
+  'scroll',
+  function handleScroll() {
+    const scrollTopPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTopPosition > lastScrollTop) {
+      console.log('scrolling down');
+
+     }
+     else if (scrollTopPosition < lastScrollTop) {
+      console.log('scrolling up');
+    }
+    lastScrollTop =
+      scrollTopPosition <= 0 ? 0 : scrollTopPosition;
+  },
+  false,
+  
+);
